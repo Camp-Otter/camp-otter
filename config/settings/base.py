e71c6b6d9494ff02e2a0c8bb59290a63b0b91d82
paulopperman <paul.opperman@gmail.com>
2018-04-21 20:58:01 -0400

    configure postgis database

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,6 +39,7 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
 ]
 
 THIRD_PARTY_APPS = [
@@ -85,10 +86,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# Database is setup to connect to a networked docker container running postgis.
+# TODO: this needs to be updated if using docker compose
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'geodjango',
+        'USER': 'postgres',
+        'PASSWORD': 'testdjango',
+        'HOST': 'localhost',
+        'PORT': '8080',
     }
 }
 
